b525fb416c7e74baa7aad63b5b46bda1f0f3e13d
mubeenhassan <mubeenhassan34@gmail.com>
2025-04-25 14:38:58 +0500

    quick fix

--- components/sections/dashboard/profile/tabs/report-scores/index.jsx
+++ components/sections/dashboard/profile/tabs/report-scores/index.jsx
@@ -1,22 +1,37 @@
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 import ReportSection from "../profile-score/report-section";
 import ReportList from "./report-list";
 
 const ContentLeft = ({ profile }) => {
   const searchParams = useSearchParams();
-  const activeReport = searchParams.get('reprot');
+  const activeReport = searchParams.get("reprot");
 
-  return <ReportSection reports={profile.reports} profileId={profile.id}  activeReport={activeReport}/>;
+  return (
+    <ReportSection
+      reports={profile.reports}
+      profileId={profile.id}
+      activeReport={activeReport}
+    />
+  );
 };
 
 const ContentRight = ({ profile }) => {
   const searchParams = useSearchParams();
-  const activeReport = searchParams.get('reprot');
-  return(
-  <>
-    <ReportList reportList={profile.reports[activeReport-1].reportDetails} />
-  </>
-)};
+  const activeReport = searchParams.get("reprot");
+  const activeReportDetails = profile.reports.find(
+    (report) => report.id === activeReport
+  )?.reportDetails;
+
+  return (
+    <>
+      {activeReportDetails && activeReportDetails.length > 0 ? (
+        <ReportList reportList={activeReportDetails} />
+      ) : (
+        <p className="text-sm text-center py-6 text-gray-500 italic">No list found</p>
+      )}
+    </>
+  );
+};
 
 const ReportScores = {
   ContentLeft,
